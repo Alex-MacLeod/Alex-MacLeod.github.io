20b98cff1700ae56dd7cf5895c33f9e46617c09f
Alexander MacLeod <30347591+AlexMacLeod23@users.noreply.github.com>
2017-09-11 08:36:57 +0100

    Working on portfolio page

--- src/webapp/app/features/projects/projectsController.js
+++ src/webapp/app/features/projects/projectsController.js
@@ -3,6 +3,13 @@
 (function() {
 
     var ProjectsController =  function() {
+        var vm = this;
+
+        vm.barrenMoorImg = "assets/img/portfolio/thumbnails/1.jpg";
+
+        vm.hangmanImg = "assets/img/portfolio/thumbnails/2.jpg";
+
+        vm.apolloCinemasImg = "assets/img/portfolio/thumbnails/3.jpg";
 
     };
 
