6007a1abe0e2b58b6a5dc6132e71242348d216ce
Alexander MacLeod <30347591+AlexMacLeod23@users.noreply.github.com>
2018-02-20 22:31:19 +0000

    Removed OnInit

--- src/app/career/career.component.ts
+++ src/app/career/career.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-career',
   templateUrl: './career.component.html',
   styleUrls: ['./career.component.css']
 })
-export class CareerComponent implements OnInit {
+export class CareerComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
