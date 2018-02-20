0af5a786a04caa1324071745f6b4593e88be1955
Alexander MacLeod <30347591+AlexMacLeod23@users.noreply.github.com>
2018-02-20 22:31:53 +0000

    Improved typing

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -7,7 +7,7 @@ import { Message } from './message';
 
 const httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' })
-  };
+};
 
 @Injectable()
 export class ContactService {
@@ -16,12 +16,12 @@ export class ContactService {
         private http: HttpClient
     ) { }
 
-    private googleScript = "https://script.google.com/macros/s/AKfycbzCVcDrHI_yn2LPjr45k7r-E5Sw6PKTVph8jmNJTz2FtHAGkNne/exec";
+    private googleScript: string = "https://script.google.com/macros/s/AKfycbzCVcDrHI_yn2LPjr45k7r-E5Sw6PKTVph8jmNJTz2FtHAGkNne/exec";
     
     sendMessage(message: Message): Promise<Response> {
-        let encoded = Object.keys(message).map(function (k) {
-            return encodeURIComponent(k) + "=" + encodeURIComponent(message[k]);
-            }).join("&");
+        let encoded = Object.keys(message)
+            .map((k) => encodeURIComponent(k) + "=" + encodeURIComponent(message[k]))
+            .join("&");
         return this.http.post<Response>(this.googleScript, encoded, httpOptions).toPromise()
     }
 }
