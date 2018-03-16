7ce3ed98476ffe0ac4460c6a530d568cf7dadb9c
Alexander MacLeod <30347591+AlexMacLeod23@users.noreply.github.com>
2018-03-16 01:39:23 +0000

    Refactored app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap/alert';
 import { ReactiveFormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
 import { CareerComponent } from './career/career.component';
 import { ContactComponent } from './contact/contact.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { HomeComponent } from './home/home.component';
 import { InterestsComponent } from './interests/interests.component';
-import { ContactService } from './contact.service';
 import { EducationComponent } from './education/education.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { SkillsComponent } from './skills/skills.component';
 import { HangmanComponent } from './hangman/hangman.component';
+
+import { ContactService } from './contact.service';
 import { HangmanService } from './hangman.service';
 
 
