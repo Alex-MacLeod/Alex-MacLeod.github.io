9c59aa4898577867774a13ac377f45e34fbc48bb
Alexander MacLeod <30347591+AlexMacLeod23@users.noreply.github.com>
2019-01-11 21:22:20 +0000

    Removed unused ng bootstrap modules

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 import {APP_BASE_HREF} from '@angular/common';
@@ -6,26 +6,26 @@ import {APP_BASE_HREF} from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AlertModule } from 'ngx-bootstrap/alert';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
-import { PaginationModule } from 'ngx-bootstrap/pagination';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { HomeComponent } from './home/home.component';
-import { CareerComponent } from './career/career.component';
-import { EducationComponent } from './career/education/education.component';
-import { ExperienceComponent } from './career/experience/experience.component';
-import { SkillsComponent } from './career/skills/skills.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
 import { ContactComponent } from './contact/contact.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { HangmanComponent } from './portfolio/hangman/hangman.component';
 import { InterestsComponent } from './interests/interests.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
-        CareerComponent,
+        AboutComponent,
         ContactComponent,
         PortfolioComponent,
         HomeComponent,
@@ -38,28 +38,35 @@ describe('AppComponent', () => {
       imports: [
         AppRoutingModule,
         ReactiveFormsModule,
-        AlertModule.forRoot(),
-        CarouselModule.forRoot(),
-        PaginationModule.forRoot(),
-        CollapseModule.forRoot(),
+        AlertModule.forRoot()
       ],
       providers: [{provide: APP_BASE_HREF, useValue : '/' }]
     }).compileComponents();
   }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+});
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  // it(`should have as title 'app'`, async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('app');
-  // }));
-  // it('should render title in a h1 tag', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  // }));
+
+  it('should have a nav bar', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('nav')).toBeTruthy();
+  }));
+
+  it('should have a footer', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('footer')).toBeTruthy();
+  }));
+
+  it('should have a routing component', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
+  }));
 });
